02c3c579dadca2c66d6a840c055cfbd1146b8f9b
Patrik Tomek <pataz23@seznam.cz>
2022-07-14 18:14:47 +0200

    Desktop final styling

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 (function ($) {
   $(".accordion-question").click(function () {
     if ($(this).next(".accordion-answer").hasClass("open")) {
-
-        $(this).removeClass('active')
+      $(this).removeClass("active");
       $(this).next(".accordion-answer").removeClass("open").slideUp();
-
+      $(this).children(".arrow").removeClass("reversed-arrow");
     } else {
-         $('.accordion-question').removeClass('active');
+      $(".accordion-question").removeClass("active");
       $(".accordion-answer").removeClass("open").slideUp();
+      $(".arrow").removeClass("reversed-arrow");
       $(this).addClass("active");
       $(this).next(".accordion-answer").addClass("open").slideDown();
-
+      $(this).children(".arrow").addClass("reversed-arrow");
     }
   });
 })(jQuery);
